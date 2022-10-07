3dda3c7d7ab9df65820508770c01e3903f6875a5
BobbyWasabiReal <wasabibob13@gmail.com>
2022-10-07 03:03:50 +0300

    Project-1-Browser-Based-Game

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Project-1-Browser-Based-Game
+
+1.  Define the constants (audio)
+
+2. Define the required variables
+
+ (Define the board, turn/score, mistake/lose)
+
+3. Cache the score element for later use
+ (Play again button, and score)
+
+4. Add needed event listeners
+ (One for the board that on click will see
+if the player's choice matches the computer's, and one that )
+
+5. Write functions: 
+
+1. Computer makes a random choice, and store it.
+2. play back the stored pattern (with audio).
---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/
+// Define the constants (audio)
+
+/*----- state variables -----*/
+// Define the required variables
+// Define the board, turn/score, mistake/lose
+
+/*----- cached elements  -----*/
+// Cache the score element for later use
+// (Play again button, and score)
+
+/*----- event listeners -----*/
+// Add needed event listeners
+// (One for the board that on click will see
+// if the player's choice matches the computer's, and
+// one that )
+
+/*----- functions -----*/
+//
