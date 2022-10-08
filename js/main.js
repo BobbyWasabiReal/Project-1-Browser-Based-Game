7f19bf33be8ab5634b454822ae17de4cb2b63f8b
BobbyWasabiReal <wasabibob13@gmail.com>
2022-10-08 03:12:45 +0300

    laying the foundation for the necessary functions

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
 /*----- constants -----*/
 // Define the constants (audio)
+const AUDIO = new Audio();
+const BOARD = {
+    0: "green",
+    1: "red",
+    2: "yellow",
+    3: "blue"
+};
 
 /*----- state variables -----*/
 // Define the required variables
 // Define the board, turn/score, mistake/lose
-let board;
+let pSequence;
+let sequence;
 let score;
-let complete;
+let loss;
 
 /*----- cached elements  -----*/
-// Cache the score element for later use
+// Cache the elements for later use
 // (Play again button, and score)
+const playAgainBtn = document.getElementById('play-again');
+const scoreEl = document.getElementById("score");
 
 /*----- event listeners -----*/
 // Add needed event listeners
 // (One for the board that on click will see
 // if the player's choice matches the computer's, and
 // one that )
+
 
 /*----- functions -----*/
 // Define functions
@@ -25,14 +36,39 @@ let complete;
 init();
 
 function init() {
-    board = [
-        "green", "red", "yellow", "blue"
-    ];
+    // Computer will generate a random num 0 - 3
+    sequence = [];
+    pSequence = [];
     score = 0;
-    complete = "false";
+    loss = false;
     render();
 }
 
-function render() {
+function renderLoss() {
+    if (pSequence !== sequence) {
+        loss = true;
+    } else {
+        loss = false;
+    }
+    // Button visibility
+    playAgainBtn.style.visibility = loss ? "hidden" : "visible";
+}
+
+function renderScore() {
+    // If choice correct add to the score
+    if (pSequence === sequence) {
+        return score++;
+    }
+    render();
+}
+
+// Replay the sequence
+function renderSquence() {
     
+}
+
+function render() {
+    renderLoss();
+    renderScore();
+    renderSequence();
 }
