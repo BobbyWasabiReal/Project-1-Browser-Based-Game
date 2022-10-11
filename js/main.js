e22ad07f4db555896d302b3ab333531986c98504
BobbyWasabiReal <wasabibob13@gmail.com>
2022-10-12 02:45:59 +0300

    Thank you Devlin

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 /*----- constants -----*/
 // Define the constants (audio)
-const AUDIO = new Audio();
+const AUDIO = {
+    green: new Audio(),
+    red: new Audio(),
+    yellow: new Audio(),
+    blue: new Audio()
+};
+
 const BOARD = {
     0: "green",
     1: "red",
     2: "yellow",
     3: "blue"
 };
+
 const equals = (pSequence, sequence) =>
   pSequence.length === sequence.length &&
   pSequence.every((v, i) => v === sequence[i]);
@@ -32,6 +39,10 @@ const scoreEl = document.getElementById("score");
 // (One for the board that on click will see
 // if the player's choice matches the computer's and one for the play agin button)
 playAgainBtn.addEventListener("click", init);
+document.getElementById("green").addEventListener("click", handleChoice);
+document.getElementById("red").addEventListener("click", handleChoice);
+document.getElementById("yellow").addEventListener("click", handleChoice);
+document.getElementById("blue").addEventListener("click", handleChoice);
 
 /*----- functions -----*/
 // Define functions
@@ -48,8 +59,12 @@ function init() {
     render();
 }
 
-function handleChoice(evt)  {
-    
+function handleChoice(evt) {
+    const board = Object.values(BOARD);
+    if (equals(pSequence, sequence) === true && loss === false && isPlayingSequence ===false) {
+        pSequence.push(":(");
+        console.log(pSequence);
+    }
 }
 
 function compareSequnce() {
@@ -67,10 +82,10 @@ function comChoice() {
 }
 
 function renderLoss() {
-    if (equals(pSequence, sequence) === true) {
-        loss = false;
-    } else {
+    if (equals(pSequence, sequence) === false) {
         loss = true;
+    } else {
+        loss = false;
     }
     // Button visibility
     playAgainBtn.style.visibility = loss ? "visible" : "hidden";
@@ -87,12 +102,42 @@ function renderScore() {
 
 // Replay the sequence
 function renderSequence() {
-    if (equals(pSequence, sequence) === true) {
-        // comChoice();
+    if (equals(pSequence, sequence) === true && isPlayingSequence === false) {
+        comChoice();
         isPlayingSequence = true;
+
+
+
+        function displayText(str) {
+            console.log(str);
+         }
+         var i = 0;
+         
+         var a = [12, 3, 45, 6, 7, 10];
+         
+         function timedLoop() {
+         setTimeout(function () {
+             displayText(sequence[i]);
+             i++;
+             if(i < sequence.length) {
+                 timedLoop();
+             }
+         }, 2000)
+         }
+         
+         timedLoop();
+
+
         for (let i = 0; i < sequence.length; i++) {
             if (sequence[i] === "green") {
-                greenFlash();
+                // greenFlash();
+                const greenBtn = document.getElementById("green");
+                greenBtn.style.borderColor = "#18a830";
+                greenBtn.style.backgroundColor = "#1cd440";
+                setTimeout(() => {
+                    greenBtn.style.borderColor = "#1cd440";
+                    greenBtn.style.backgroundColor = "#18a830";
+                }, 3000);
             } else if (sequence[i] === "red") {
                 redFlash();
             } else if (sequence[i] === "yellow") {
@@ -101,57 +146,49 @@ function renderSequence() {
                 blueFlash();
             }
         };
+        isPlayingSequence = false;
     } else {
         isPlayingSequence = false;
     }
 };
 
 function greenFlash() {
-    const greenBtn = document.getElementById("green");
-    setTimeout(() => {
-        greenBtn.style.borderColor = "#18a830";
-        greenBtn.style.backgroundColor = "#1cd440";
-    }, 300);
+    greenBtn.style.borderColor = "#18a830";
+    greenBtn.style.backgroundColor = "#1cd440";
     setTimeout(() => {
         greenBtn.style.borderColor = "#1cd440";
         greenBtn.style.backgroundColor = "#18a830";
-    }, 500);
+    }, 3000);
 }
 
 function redFlash() {
     const redBtn = document.getElementById("red");
-    setTimeout(() => {
-        redBtn.style.backgroundColor = "#c74b40";
-        redBtn.style.borderColor = "#993026";
-    }, 300);
+    redBtn.style.backgroundColor = "#c74b40";
+    redBtn.style.borderColor = "#993026";
     setTimeout(() => {
         redBtn.style.backgroundColor = "#993026";
         redBtn.style.borderColor = "#c74b40";
-    }, 500);
+    }, 3000);
 }
 
 function yellowFlash() {
     const yellowBtn = document.getElementById("yellow");
-    setTimeout(() => {
-        yellowBtn.style.backgroundColor = "#fcfa81";
-        yellowBtn.style.borderColor = "#d6d300";
-    }, 300);
+    yellowBtn.style.backgroundColor = "#fcfa81";
+    yellowBtn.style.borderColor = "#d6d300";
     setTimeout(() => {
         yellowBtn.style.backgroundColor = "#d6d300";
         yellowBtn.style.borderColor = "#fcfa81";
-    }, 500);
+    }, 3000);
 }
 
 function blueFlash() {
     const blueBtn = document.getElementById("blue");
-    setTimeout(() => {
-        blueBtn.style.backgroundColor = "#477bd6";
-        blueBtn.style.borderColor = "#113d8a";
-    }, 300);
+    blueBtn.style.backgroundColor = "#477bd6";
+    blueBtn.style.borderColor = "#113d8a";
     setTimeout(() => {
         blueBtn.style.backgroundColor = "#113d8a";
         blueBtn.style.borderColor = "#477bd6";
-    }, 500);
+    }, 3000);
 }
 
 function render() {
